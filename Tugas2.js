4e52df8f488138d9e3c27b8250a97ffe4cad6330
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-10 19:36:54 +0700

    string, integer, boolean, null, undefined dan array
    string, integer, boolean, null, undefined dan array

---
+++ Tugas2.js
@@ -0,0 +1,13 @@
+var number = 3;
+var string = "Manusia";
+var boolean = true;
+var tidak_ada = null;
+var Undefined;
+var Array = [1, 2, 3, 4, 5];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(tidak_ada);
+console.log(Undefined);
+console.log(Array[4]);
